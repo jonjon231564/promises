444271c3073c1145a2eb3a129b4c53444c296b40
jonjon231564 <137326741+jonjon231564@users.noreply.github.com>
2024-06-11 16:10:32 -0500

    commit 1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 
 import './style.css'
+
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 2
+    if (a == 2) {
+        resolve('success')
+    } else {
+        reject('failed')
+    }
+})
+
+p.then((message) => {
+    console.log('this is in the then ' + message)
+}).catch((message) => {
+    console.log('this is in the catch ' + message)
+})
